8d1938e5171d368a6890bb38e1a6968e95e0c828
김주한 <80670283+kjh7330@users.noreply.github.com>
2021-04-26 13:15:05 +0900

    버스 정류장 추천 알고리즘 완성

---
+++ EBTD_ver1.0/src/main/java/com/ebtd/www/bean/BusVO.java
@@ -0,0 +1,35 @@
+package com.ebtd.www.bean;
+
+import com.ebtd.www.bean.BusVO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor //생성자 만듦
+@NoArgsConstructor //default 생성자 만듦
+public class BusVO {
+	private String busNum;
+	private int stopRouteNum1;
+	private int stopRouteNum2;
+	private int stopRouteNum3;
+	private int stopRouteNum4;
+	private int stopRouteNum5;
+	private int stopRouteNum6;
+	private int stopRouteNum7;
+	private int stopRouteNum8;
+	private int stopRouteNum9;
+	private int stopRouteNum10;
+	private int stopRouteNum11;
+	private int stopRouteNum12;
+	private int stopRouteNum13;
+	private int stopRouteNum14;
+	private int stopRouteNum15;
+	private int stopRouteNum16;
+	private int stopRouteNum17;
+	private int stopRouteNum18;
+	private int stopRouteNum19;
+	private int stopRouteNum20;
+	
+}
