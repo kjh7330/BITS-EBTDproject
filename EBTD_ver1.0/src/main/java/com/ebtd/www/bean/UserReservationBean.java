8031f952dadf72366266b72e30143e7c20a97bd8
Shin <jaekoo109@naver.com>
2021-05-07 11:06:20 +0900

    빈 b-no 타입변경

--- EBTD_ver1.0/src/main/java/com/ebtd/www/bean/UserReservationBean.java
+++ EBTD_ver1.0/src/main/java/com/ebtd/www/bean/UserReservationBean.java
@@ -16,7 +16,7 @@ public class UserReservationBean {
 	private String u_username;
 	private int s_nostart;
 	private int s_nolast;
-	private int b_no;
+	private String b_no;
 	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
 	private Timestamp ur_date;
 	private int ur_state;
